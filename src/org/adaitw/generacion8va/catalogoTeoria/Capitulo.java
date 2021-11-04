daa586cf69527ff33ae4c41181758f1fbfb9972e
Mariana Madeira <mrn.m.92@gmail.com>
2021-11-04 16:01:05 -0300

    Comienzo Template Archivos

--- src/org/adaitw/generacion8va/catalogoTeoria/Capitulo.java
+++ src/org/adaitw/generacion8va/catalogoTeoria/Capitulo.java
@@ -4,18 +4,24 @@ import java.util.LinkedList;
 
 public class Capitulo {
 	private int nroCapitulo;
+	private String nombre;
 	private LinkedList<Concepto> conceptos;
 	private String dificultad;
 
-	public Capitulo(int nroCapitulo, String dificultad) {
+	public Capitulo(int nroCapitulo, String nombre, String dificultad) {
 		super();
 		this.nroCapitulo = nroCapitulo;
+		this.nombre = nombre;
 		this.conceptos = new LinkedList<Concepto>();
 		this.dificultad = dificultad;
 	}
 
 	public int getNroCapitulo() {
 		return nroCapitulo;
+	}
+
+	public String getNombre() {
+		return nombre;
 	}
 
 	public LinkedList<Concepto> getConceptos() {
---
+++ src/org/adaitw/generacion8va/lectores/LectorCatalogo.java
@@ -0,0 +1,28 @@
+package org.adaitw.generacion8va.lectores;
+
+import java.util.LinkedList;
+
+import org.adaitw.generacion8va.catalogoTeoria.Capitulo;
+
+public class LectorCatalogo {
+
+	public static LinkedList<Capitulo> leerCapitulos() {
+		LinkedList<Capitulo> capitulos = new LinkedList<Capitulo>();
+		
+		
+		
+		return capitulos;
+	}
+
+	public static void leerConceptos() {
+
+	}
+
+	public static void leerEjemplos() {
+
+	}
+	
+	public static void leerPreguntas() {
+
+	}
+}
