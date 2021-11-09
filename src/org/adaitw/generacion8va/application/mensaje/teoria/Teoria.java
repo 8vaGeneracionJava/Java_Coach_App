ee7d7b372c2538414dbc61a20c51cf71878db2ee
GabrielaSoruco <soruco.gabi@gmail.com>
2021-11-09 18:51:27 -0300

    creacion del paquete mensaje y sus clases

---
+++ src/org/adaitw/generacion8va/application/mensaje/teoria/Teoria.java
@@ -0,0 +1,22 @@
+package org.adaitw.generacion8va.application.mensaje.teoria;
+
+import org.adaitw.generacion8va.application.mensaje.Mensaje;
+
+public class Teoria {
+
+    public Teoria(int nroCapitulo, int nroConcepto){
+        //String titulo = listaConceptos.get(nroConcepto).nombre;
+        //String teoria = listaConceptos.get(nroConcepto).teoria;
+    }
+
+    public Teoria(){
+        //String titulo = listaConceptos.get(Math.ramdon()*10).nombre
+    }
+    /*
+    @Override
+    public String toString() {
+        return super.toString(titulo": \n" + teoria);
+    }
+    */
+
+}
